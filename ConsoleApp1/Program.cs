c88d708d3b96587f408c07bfb2a96cdd65a9b10d
vovaigrok <index.2393@gmail.com>
2025-04-05 06:31:00 +0300

    edit

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -10,6 +10,9 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
+            int a = Convert.ToInt32(Console.ReadLine());
+            int b = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine(Math.Pow(a,b));
         }
     }
 }
